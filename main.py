a5be02224bc1c3a0cb56a4ed8b00a331a07e0a56
engrbailey <engrbailey@gmail.com>
2023-09-16 15:35:40 +0100

    Hi

---
+++ main.py
@@ -0,0 +1,29 @@
+# Selenium for web driving
+import selenium
+from selenium import webdriver
+#driver = webdriver.Chrome()  
+
+# Time for pausing between navigation
+import time
+
+# Talk to the browser 
+browser = webdriver.Chrome()
+browser.get('http://192.168.188.1')
+driver.maximize_window()
+
+# Select the password box
+password_box = driver.find_element_by_id("txtpwd")
+
+#typeInThePassword
+password_box.send_keys('admin')
+
+#findingAndClickingLoginButton
+#login_button = driver.find_element_by_id('btnLogin')
+#login_button.click()
+
+#click on advanced
+
+
+
+#closeBrowser
+#browser.quit()
